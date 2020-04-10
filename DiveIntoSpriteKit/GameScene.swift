4f2a4778b6074d9b8d121607e2f7454431c95cd2
Keira Hartley <khartley@lcs.on.ca>
2020-04-10 16:08:34 -0400

    Space Dust now goes across the screen

--- DiveIntoSpriteKit/GameScene.swift
+++ DiveIntoSpriteKit/GameScene.swift
@@ -10,23 +10,30 @@ import SpriteKit
 
 @objcMembers
 class GameScene: SKScene {
+    
     override func didMove(to view: SKView) {
         // this method is called when your game scene is ready to run
         
         let background = SKSpriteNode(imageNamed: "space.jpg")
         background.zPosition = -1
         addChild(background)
-
+        
+        if let particles = SKEmitterNode(fileNamed: "SpaceDusk") {
+            particles.advanceSimulationTime(10)
+            particles.position.x = 512
+            addChild(particles)
+        }
+        
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user touches the screen
     }
-
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user stops touching the screen
     }
-
+    
     override func update(_ currentTime: TimeInterval) {
         // this method is called before each frame is rendered
     }
