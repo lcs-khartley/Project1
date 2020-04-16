417ab8fa1baa8d8233daeb9692f0d8117d39b63d
Keira Hartley <khartley@lcs.on.ca>
2020-04-16 10:37:30 -0400

    Added personalized assets.

--- DiveIntoSpriteKit/GameScene.swift
+++ DiveIntoSpriteKit/GameScene.swift
@@ -11,27 +11,65 @@ import SpriteKit
 @objcMembers
 class GameScene: SKScene {
     
+    
+    let player = SKSpriteNode(imageNamed: "player-ufo.png")
+    var touchingPlayer = false
+    
     override func didMove(to view: SKView) {
         // this method is called when your game scene is ready to run
         
+        // set the background picture
         let background = SKSpriteNode(imageNamed: "space.jpg")
         background.zPosition = -1
         addChild(background)
         
+        // add the space dust particles that go across the screen
         if let particles = SKEmitterNode(fileNamed: "SpaceDusk") {
             particles.advanceSimulationTime(10)
             particles.position.x = 512
             addChild(particles)
+            
         }
+        
+        // set the player position on the screen
+        player.position.x = -400
+        player.zPosition = 1
+        addChild(player)
+        
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user touches the screen
+        guard let touch = touches.first else{
+            return }
+        
+        // find if the player touched the player icon
+        let location = touch.location(in: self)
+        let tappedNodes = nodes (at: location)
+        
+        if tappedNodes.contains(player) {
+            touchingPlayer = true
+        }
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard touchingPlayer else {
+            return
+        }
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let location = touch.location(in: self)
+        player.position = location
+
+    }
+    
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user stops touching the screen
+        touchingPlayer = false
     }
     
     override func update(_ currentTime: TimeInterval) {
